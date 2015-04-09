ee6fe6174c733ac2a45cbf86bf56afde74147319
Lukas Hinsch <lukas@hinsch.eu>
2015-04-09 09:30:05 +0200

    Ensure stable behavior with listener order
    - force re-init of logging system after properties set (as it's not possible to reliably do it between environment properties available and spring boot default logging listener)
    - require spring boot >= 1.2.1

--- src/main/java/eu/hinsch/spring/boot/logging/ExtendedLoggingPropertiesListener.java
+++ src/main/java/eu/hinsch/spring/boot/logging/ExtendedLoggingPropertiesListener.java
@@ -2,7 +2,8 @@ package eu.hinsch.spring.boot.logging;
 
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.boot.logging.LoggingApplicationListener;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 
@@ -11,13 +12,20 @@ import java.util.Map;
 /**
  * Created by lh on 06/04/15.
  */
-public class ExtendedLoggingPropertiesListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
+public class ExtendedLoggingPropertiesListener extends LoggingApplicationListener {
 
     private static final String KEY_PREFIX = "logging.properties.";
     private static final String PROPERTY_PREFIX = "LOGGING_";
 
     @Override
-    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationEnvironmentPreparedEvent) {
+            addLoggingProperties((ApplicationEnvironmentPreparedEvent) event);
+        }
+        super.onApplicationEvent(event);
+    }
+
+    public void addLoggingProperties(ApplicationEnvironmentPreparedEvent event) {
         ConfigurableEnvironment environment = event.getEnvironment();
 
         new RelaxedPropertyResolver(environment)
@@ -38,12 +46,13 @@ public class ExtendedLoggingPropertiesListener implements ApplicationListener<Ap
     }
 
     /**
-      * TODO this must run before org.springframework.boot.logging.LoggingApplicationListener (+11)
+      * ideally this would be run before org.springframework.boot.logging.LoggingApplicationListener (+11)
       * but after org.springframework.boot.context.config.ConfigFileApplicationListener (+10)
-     * so it might be entirely accidental that this actually works
+      * so we could simply insert the properties in between those steps, but unfortunately there is
+      * no guaranteed way to do that, so we run afterwards and re-initialize the entire thing via super class.
       */
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE + 11;
+        return Ordered.HIGHEST_PRECEDENCE + 12;
     }
 }
